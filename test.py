53f658ec268babc27b47aadd7a1a3f306b1d8777
ken748322 <ken.748322@gmail.com>
2020-10-07 14:17:21 +0900

    test for reading .txt file

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import os
 import numpy as np 
 
 
-rmse = np.array([["" for j in range(20)] for i in range(5, 51, 5)])
+rmse = np.array([[0.0 for j in range(20)] for i in range(5, 51, 5)])
 
 
 i = 0 
 for fpfh_radius_size in range(5, 51, 5):
-    with  open('result/result_rmse/result_rmse_radius%d.txt' % fpfh_radius_size, mode="r") as f:
-        rmse[i] = np.array([s.strip() for s in f.readlines()], dtype='float64')
+    with  open('result/result_rmse/result_rmse_radius%d.txt' % fpfh_radius_size, mode='r') as f:
+        rmse[i] = np.array([s.strip() for s in f.readlines()])
     i = i + 1
 
 
