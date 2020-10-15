0bb6f22e2530dca70b4181c4381a162bf0f5f2a3
ken748322 <ken.748322@gmail.com>
2020-10-15 14:30:51 +0900

    add save_img function

--- preprocess.py
+++ preprocess.py
@@ -15,6 +15,19 @@ def draw_registration_result(source, target, transformation = np.identity(4)):
 
 
 
+def save_img(source, target, file_path, file_name, transformation = np.identity(4)):
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(source)
+    vis.add_geometry(target)
+    vis.update_geometry()
+    vis.poll_events()
+    vis.update_renderer()
+    vis.capture_screen_image(file_path + "/" + file_name + ".jpg")
+    vis.destroy_window()
+
+
+
 class prepare_dataset:
 
     def __init__(self, file_pass):
@@ -60,3 +73,4 @@ def execute_global_registration(source_down, target_down, source_fpfh, target_fp
         o3d.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)],
         criteria=o3d.registration.RANSACConvergenceCriteria(max_iter, max_valid)) 
     return result
+
